f3c506773aa45b37b2a6b8fdbf6204c7324dff54
JonathanLeaper <jonathan.leaper@ontariotechu.net>
2026-01-28 23:35:52 -0500

    project compleated

---
+++ src/test/java/com/ontariotechu/sofe3980U/AppTest.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.sofe3980U;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
+}
